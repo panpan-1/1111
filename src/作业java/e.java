e42578dbb3816dcfc3be2ec0b4cd4788d139b487
panpan-1 <893803035@qq.com>
2021-10-17 23:16:42 +0800

    26

---
+++ src/作业java/e.java
@@ -0,0 +1,12 @@
+package ×÷Òµjava;
+
+public class e {
+
+	public static void main(String[] args) {
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven!");
+
+	}
+
+}
