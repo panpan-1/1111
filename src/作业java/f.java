a2dda034be1410666d7f607adcf9ff6ca1628a66
panpan-1 <893803035@qq.com>
2021-10-17 23:22:38 +0800

    333

---
+++ src/作业java/f.java
@@ -0,0 +1,11 @@
+package ×÷Òµjava;
+
+public class f {
+
+	public static void main(String[] args) {
+		System.out.print("(10.5+2*3)/(45-3.5)=");
+		System.out.println((10.5+2*3)/(45-3.5));
+
+	}
+
+}
