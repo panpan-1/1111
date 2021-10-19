39f815123ce7d46a450c83a998e37c1f2296fa21
panpan-1 <893803035@qq.com>
2021-10-19 13:46:25 +0800

    3

--- src/作业java/k.java
+++ src/作业java/k.java
@@ -11,7 +11,7 @@ public class k {
 		double number2 =input.nextDouble();
 		double number3 =input.nextDouble();
 		double average = (number1+number2+number3)/3;
-		System.out.println("The average of" +number1+ " "+number2 + " " +" is "+number3+"is" +average);
+		System.out.println("The average of" +number1+ " "+number2 + " " +number3+"is" +average);
 
 	}
 
