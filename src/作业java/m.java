d2a67061376cd00f68bfa61ba88048bb82fe5d70
panpan-1 <893803035@qq.com>
2021-10-19 13:52:18 +0800

    1e2

---
+++ src/作业java/m.java
@@ -0,0 +1,17 @@
+package ×÷Òµjava;
+
+import java.util.Scanner;
+
+public class m {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		System.out.print("Enter a integer for seconds:");
+		int seconds = input.nextInt();
+		int minutes = seconds / 60;
+		int remainingSeconds = seconds %60;
+		System.out.println(seconds + " seconds is " + minutes + " minutes and " + remainingSeconds + " seconds");
+
+	}
+
+}
