70f323cd50f0cbc6a7424e62c2bb6ef454bc6040
panpan-1 <893803035@qq.com>
2021-11-23 13:54:57 +0800

    程序清单9-8 9-9

---
+++ src/作业java/CircleWithPrivateDataFields.java
@@ -0,0 +1,25 @@
+package ×÷Òµjava;
+
+public class CircleWithPrivateDataFields {
+	private double radius = 1;
+    private  static int numberOfObjects = 0;
+    public CircleWithPrivateDataFields(){
+        numberOfObjects ++;
+    }
+    public CircleWithPrivateDataFields(double newRadius){
+        radius = newRadius;
+        numberOfObjects ++;
+    }
+    public void setRadius(double newRadius){
+        radius = (newRadius >= 0) ? newRadius : 0;
+    }
+    public static int getNumberOfObjects(){
+        return numberOfObjects;
+    }
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+    public double getRadius() {
+    	 return radius;
+    }
+}
