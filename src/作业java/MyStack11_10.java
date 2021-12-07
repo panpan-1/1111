6d69ec5c61ae2af9214d367a0398800015753ca5
panpan-1 <91315789+panpan-1@users.noreply.github.com>
2021-12-07 14:40:20 +0800

    Update MyStack11_10.java

--- src/作业java/MyStack11_10.java
+++ src/作业java/MyStack11_10.java
@@ -1,5 +1,27 @@
-package ×÷Òµjava;
+package ä½œä¸šjava;
 
+import java.util.ArrayList;
 public class MyStack11_10 {
+    private ArrayList<Object> list = new ArrayList<>();
+    public boolean isEmpty(){
+        return list.isEmpty();
+    }
 
+    public int getSize(){
+        return list.size();
+    }
+
+    public Object pop(){
+        Object o = list.get(getSize() - 1);
+        list.remove(getSize() - 1);
+        return o;
+    }
+
+    public void push(Object o){
+        list.add(o);
+    }
+
+    public String toString(){
+        return "stack: " + list.toString();
+    }
 }
