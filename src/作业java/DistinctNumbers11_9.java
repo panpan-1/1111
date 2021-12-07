8f8f9393c0398efe8a38e3e11bb566c595125d8d
panpan-1 <91315789+panpan-1@users.noreply.github.com>
2021-12-07 14:40:06 +0800

    Update DistinctNumbers11_9.java

--- src/作业java/DistinctNumbers11_9.java
+++ src/作业java/DistinctNumbers11_9.java
@@ -1,5 +1,21 @@
-package ×÷Òµjava;
+package ä½œä¸šjava;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 public class DistinctNumbers11_9 {
-
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter integers (input ends with 0): ");
+        int value;
+        do{
+            value = input.nextInt();
+            if(!list.contains(value) && value != 0)
+                list.add(value);
+        }while (value != 0);
+        for(int i = 0; i < list.size();++ i){
+            System.out.print(list.get(i) + " ");
+        }
+    }
 }
+
