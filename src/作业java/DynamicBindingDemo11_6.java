89e6c3774781b37268e0f257b749969a13c52bfd
panpan-1 <91315789+panpan-1@users.noreply.github.com>
2021-12-07 14:39:05 +0800

    Update DynamicBindingDemo11_6.java

--- src/作业java/DynamicBindingDemo11_6.java
+++ src/作业java/DynamicBindingDemo11_6.java
@@ -1,5 +1,28 @@
-package ×÷Òµjava;
+package ä½œä¸šjava;
 
 public class DynamicBindingDemo11_6 {
-
+    public static void main(String[] args) {
+        m(new GraduateStudent());
+        m(new Student());
+        m(new Person());
+        m(new Object());
+    }
+    public static void m(Object x){
+        System.out.println(x.toString());
+    }
+}
+class GraduateStudent extends Student{
+	
+}
+class Person extends Object{
+    @Override
+    public String toString(){
+        return "Person";
+    }
+}
+class Student extends  Person{
+    @Override
+    public String toString(){
+        return "Student";
+    }
 }
