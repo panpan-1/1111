71185542bc85d360ac2fc96194f288b0c0a89442
panpan-1 <91315789+panpan-1@users.noreply.github.com>
2021-12-07 14:38:31 +0800

    Update TestCircleRectangle11_4.java

--- src/作业java/TestCircleRectangle11_4.java
+++ src/作业java/TestCircleRectangle11_4.java
@@ -1,5 +1,19 @@
-package ×÷Òµjava;
+package ä½œä¸šjava;
+
+import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
 
 public class TestCircleRectangle11_4 {
+    public static void main(String[] args) {
+    	CircleFromSimpleGeometricObject11_2 circle = new CircleFromSimpleGeometricObject11_2();
+        System.out.println("A circle " + circle.toString());
+        System.out.println("The color is " + circle.getColor());
+        System.out.println("The radius is " + circle.getColor());
+        System.out.println("The area is " + circle.getArea());
+        System.out.println("The diameter is " + circle.getDiameter());
 
+        RectangleFromSimpleGeometricObject11_3 rectangle = new RectangleFromSimpleGeometricObject11_3(2,4);
+        System.out.println("\nA rectangle " + rectangle.toString());
+        System.out.println("The area is " + rectangle.getArea());
+        System.out.println("The perimeter is " + rectangle.getPerimeter());
+    }
 }
