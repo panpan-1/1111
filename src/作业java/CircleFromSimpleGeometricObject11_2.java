7a37661562f4022a680f0187cdba7697d1a3777b
panpan-1 <91315789+panpan-1@users.noreply.github.com>
2021-12-07 14:37:19 +0800

    Update CircleFromSimpleGeometricObject11_2.java

--- src/作业java/CircleFromSimpleGeometricObject11_2.java
+++ src/作业java/CircleFromSimpleGeometricObject11_2.java
@@ -1,5 +1,37 @@
-package ×÷Òµjava;
+package ä½œä¸šjava;
 
-public class CircleFromSimpleGeometricObject11_2 {
+public class CircleFromSimpleGeometricObject11_2 extends SimpleGeometricObject11_1 {
+    private double radius;
+    public CircleFromSimpleGeometricObject11_2(){
+    }
 
+    public CircleFromSimpleGeometricObject11_2(double radius){
+        this.radius = radius;
+    }
+    public CircleFromSimpleGeometricObject11_2(double radius,String color,boolean filled){
+        this.radius = radius;
+        setColor(color);
+        setFilled(filled);
+    }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return radius * radius * Math.PI;
+    }
+
+    public double getDiameter(){
+        return 2 * radius;
+    }
+
+    public double getPerimeter(){
+        return 2 * radius * Math.PI;
+    }
+
+    public void printCircle(){
+        System.out.println("The circle is created " + getDateCreated() + " and the radius is " + radius);
+    }
 }
+
