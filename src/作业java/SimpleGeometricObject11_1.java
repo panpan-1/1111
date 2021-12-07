592c86a9454d43c4492a8049bf031de40721c46a
panpan-1 <91315789+panpan-1@users.noreply.github.com>
2021-12-07 14:35:58 +0800

    Update SimpleGeometricObject11_1.java

--- src/作业java/SimpleGeometricObject11_1.java
+++ src/作业java/SimpleGeometricObject11_1.java
@@ -1,5 +1,40 @@
-package ×÷Òµjava;
+package ä½œä¸šjava;
 
 public class SimpleGeometricObject11_1 {
+    private String color = "white";
+    private boolean filled;
+    private java.util.Date dateCreated;
+    public SimpleGeometricObject11_1(){
+        dateCreated = new java.util.Date();
+    }
 
+    public SimpleGeometricObject11_1(String color,boolean filled){
+        dateCreated = new java.util.Date();
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public  String getColor(){
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public boolean isFilled(){
+        return filled;
+    }
+
+    public void setFilled(boolean filled){
+        this.filled = filled;
+    }
+
+    public java.util.Date getDateCreated(){
+        return dateCreated;
+    }
+
+    public String toString(){
+        return "created on " + dateCreated + "\ncolor: " + color + " and filled: " + filled;
+    }
 }
